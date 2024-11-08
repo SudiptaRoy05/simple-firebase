aed0bd63a94499c4d843b37b8e1230e160cb89e4
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-08 23:58:10 +0600

    google signin popUP show properly

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,16 @@
-import { GoogleAuthProvider } from "firebase/auth/web-extension"
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../Firebase/firebase.init";
 
 export default function Login() {
     const handleGoogleSignIn = () =>{
-        console.log('google signin coming soon')
+        // console.log('google signin coming soon')
         const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result)
+        }).catch(error => {
+            console.log('ERROR',error)
+        })
     }
   return (
     <div>
