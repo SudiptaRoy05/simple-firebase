1345c61c4ed0d31f5dcf1c974cbd3cca1fb5cecf
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-09 00:16:07 +0600

    sign with google and show user data

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,36 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../../Firebase/firebase.init";
+import { useState } from "react";
 
 export default function Login() {
-    const handleGoogleSignIn = () =>{
-        // console.log('google signin coming soon')
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            console.log(result)
-        }).catch(error => {
-            console.log('ERROR',error)
-        })
-    }
+  const [user, setUser] = useState(null);
+
+  const handleGoogleSignIn = () => {
+    // console.log('google signin coming soon')
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        setUser(null);
+      });
+  };
   return (
     <div>
-      <button onClick={handleGoogleSignIn} className="btn btn-success text-white"> Login with google</button>
+      <button
+        onClick={handleGoogleSignIn}
+        className="btn btn-success text-white"
+      >
+        {" "}
+        Login with google
+      </button>
+      {user && <div>
+            <h3>{user.displayName}</h3>
+            <p>Email : {user.email}</p>
+        </div>}
     </div>
-  )
+  );
 }
